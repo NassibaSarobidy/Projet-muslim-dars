2353e922ecec2f7a5e08eb144383f14b19eaa49d
NassibaSarobidy <sarobidynassiba005@gmail.com>
2026-07-19 20:15:09 +0300

    modification du modèle contenu

--- backend/app/Models/Contenu.php
+++ backend/app/Models/Contenu.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contenu extends Model
 {
-    //
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = [
+    'titre',
+    'audio',
+    'type',
+    'user_id'
+];
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,12 +7,17 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    public function contenus(): HasMany
+{
+    return $this->hasMany(Contenu::class);
+}
 
     /**
      * The attributes that are mass assignable.
