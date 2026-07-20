d332004beca143615bb5c9c39ca039adecd13bf4
NassibaSarobidy <sarobidynassiba005@gmail.com>
2026-07-20 20:32:12 +0300

    ajout du route admin/contenus

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContenuController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\ContenuController as AdminContenuController;
 
 
 Route::get('/user', function (Request $request) {
@@ -18,4 +19,14 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/login', function () {
     return view('login');
+})->name('login');
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/admin-test', function () {
+        return "Bienvenue Admin";
+    });
+
+    Route::post('/admin/contenus', [AdminContenuController::class, 'store']);
+
 });
